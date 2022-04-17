e9cb3eeaaa4251df433384a06e448d68be955787
Ezabulbary <ezabul.bary@gmail.com>
2022-04-17 22:07:45 +0600

    product component and cart component added

---
+++ src/component/Products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = ({data}) => {
+    return (
+        <div>
+            <h2>product</h2>
+        </div>
+    );
+};
+
+export default Products;
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
+import Cart from '../Cart/Cart';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+
     return (
-        <div>
-            
+        <div className='shop-container'>
+            <div className="product-container">
+                {
+                    products.map(product => <Products
+                        key={product.id}
+                        data={product}
+                        // handleAddToCart={handleAddToCart}
+                    ></Products>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart
+                    // cart={cart}
+                    // clickDeleteToCart={clickDeleteToCart}
+                ></Cart>
+            </div>
         </div>
     );
 };
