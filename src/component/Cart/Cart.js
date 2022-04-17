cff3c31c86fac029ff7b365558fde75494fd3cdd
Ezabulbary <ezabul.bary@gmail.com>
2022-04-17 22:28:51 +0600

    random number finde

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,7 +5,23 @@ import React, { useEffect, useState } from 'react';
 import './Cart.css';
 
 const Cart = ({ cart, clickDeleteToCart }) => {
+    const [addProduct, setAddProduct] = useState(false);
+    const [freeProducts, getFreeProducts] = useState({});
 
+    const getOneForMe = () => {
+        const randomNumber = Math.floor(Math.random() * cart.length);
+        const item = cart[randomNumber];
+        getFreeProducts(item);
+    }
+
+    useEffect(() => {
+        if (cart.length > 0) {
+            setAddProduct(true);
+        }
+        else {
+            setAddProduct(false);
+        }
+    }, [cart])
 
     return (
         <div className='cart'>
@@ -31,7 +47,22 @@ const Cart = ({ cart, clickDeleteToCart }) => {
                     </div>
                 ))
             }
-
+            <div>
+                <button className={addProduct ? "get-one-btn" : "get-one-btn-disable"} onClick={getOneForMe} disabled={!addProduct}>Get One For Me
+                    <FontAwesomeIcon icon={faFaceSmile} />
+                </button>
+                {Object.keys(freeProducts).length > 0 && <div className="cart-item">
+                    <img
+                        className='cart-item-image'
+                        src={freeProducts.pairImage}
+                        alt=""
+                    />
+                    <div className='product-info'>
+                        <p>{freeProducts.name}</p>
+                        <p>Price: ${freeProducts.price}</p>
+                    </div>
+                </div>}
+            </div>
         </div>
     );
 };
