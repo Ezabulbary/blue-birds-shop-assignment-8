da0b65c4ee0fa7f19d408e8dafa9c6766f9a3bb8
Ezabulbary <ezabul.bary@gmail.com>
2022-04-17 22:21:13 +0600

    cart component added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,9 +1,37 @@
-import React from 'react';
 
-const Cart = () => {
+import { faFaceSmile, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import './Cart.css';
+
+const Cart = ({ cart, clickDeleteToCart }) => {
+
+
     return (
-        <div>
-            <h2>this is Cart</h2>
+        <div className='cart'>
+            <div className="top-bar">
+                <h3>Order Summary</h3>
+                <button className='delete' onClick={clickDeleteToCart}>
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+            </div>
+            {
+                cart.map((item, id) => (
+                    <div className="cart-item" key={id}>
+                        <img
+                            className='cart-item-image'
+                            src={item.pairImage}
+                            alt=""
+                        />
+                        <div className='product-info'>
+                            <p>{item.name}</p>
+                            <p>Price: ${item.price}</p>
+                            <p>Quantity: {item.quantity}</p>
+                        </div>
+                    </div>
+                ))
+            }
+
         </div>
     );
 };
